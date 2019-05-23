33b8e494f1363c2ddf06aebbf71a99fa9698b0ca
Gabriel Bactol <gabrielbactol109@gmail.com>
2019-05-22 19:06:26 -0700

    Deleting Google Maps unnecessary list code
    So, I was able to make Google Maps function without the DBHelper and Location list code.

--- app/src/main/java/edu/miracosta/finalprojecttest/model/GoogleMaps.java
+++ app/src/main/java/edu/miracosta/finalprojecttest/model/GoogleMaps.java
@@ -20,22 +20,9 @@ import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 import edu.miracosta.finalprojecttest.R;
-import edu.miracosta.finalprojecttest.model.GoogleMapsInformation.DBHelper;
-import edu.miracosta.finalprojecttest.model.GoogleMapsInformation.Location;
-import edu.miracosta.finalprojecttest.model.GoogleMapsInformation.LocationListAdapter;
-
-
-//TODO: (1) Implement the OnMapReadCallback interface for Google Maps
-//TODO: First, you'll need to compile GoogleMaps in build.gradle
-//TODO: and add permissions and your Google Maps API key in the AndroidManifest.xml
-
 
 public class GoogleMaps extends AppCompatActivity implements OnMapReadyCallback {
 
-    private DBHelper db;
-    private List<Location> allLocationsList;
-    private ListView locationsListView;
-    private LocationListAdapter locationListAdapter;
 
     //member variable to store google map
     private GoogleMap map;
@@ -44,13 +31,6 @@ public class GoogleMaps extends AppCompatActivity implements OnMapReadyCallback 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.google_maps);
-
-        deleteDatabase(DBHelper.DATABASE_NAME);
-        db = new DBHelper(this);
-        db.importLocationsFromCSV("locations.csv");
-
-        allLocationsList = db.getAllCaffeineLocations();
-
 
         //TODO: (2) Load the support map fragment asynchronously
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFragment);
@@ -76,26 +56,5 @@ public class GoogleMaps extends AppCompatActivity implements OnMapReadyCallback 
         //Instruct map to move to this position
         map.moveCamera(cameraUpdate);
 
-        //add all the caffeine locations to the map
-        //Loop through list and add each location
-        LatLng position;
-        for(Location location : allLocationsList)
-        {
-            position = new LatLng(location.getLatitude(), location.getLongitude());
-            map.addMarker(new MarkerOptions().position(position).title(location.getName()));
-        }
     }
-    public void viewLocationsDetails(View v)
-    {
-
-    }
-
-    // TODO: (3) Implement the onMapReady method, which will add a special "marker" for our current location,
-    // TODO: which is  33.190802, -117.301805  (OC4800 building)
-    // TODO: Then add normal markers for all the caffeine locations from the allLocationsList.
-    // TODO: Set the zoom level of the map to 15.0f
-
-
-    // TODO: (4) Create a viewLocationsDetails(View v) method to create a new Intent to the
-    // TODO: CaffeineDetailsActivity class, sending it the selectedLocation the user picked from the locationsListView
 }
